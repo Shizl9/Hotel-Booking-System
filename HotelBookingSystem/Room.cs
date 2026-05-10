6fac0406cc9864309e3847792ee57035506770c3
Sheika <sheika02x2@gmail.com>
2026-05-10 07:53:13 +0400

    add room class

---
+++ HotelBookingSystem/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingSystem
+{
+    internal class Room
+    {
+        public int Id { get; set; }
+        public string ?RoomNumber { get; set; }
+        public string ?Type { get; set; }
+        public double PricePerNight { get; set; }
+        public bool IsBooked { get; set; }
+        public int Floor { get; set; }
+    }
+}
